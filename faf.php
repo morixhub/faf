ad9121c7f83ccb1dc0d2070f53583be90869febd
Morix Dev <morix.dev@gmail.com>
2022-03-04 00:02:31 +0100

    * Added leagues management (minimal)

--- faf.php
+++ faf.php
@@ -17,11 +17,77 @@ add_action('admin_menu', 'faf_setup_menu');
 
 function faf_setup_menu() {
     add_menu_page('faf', 'faf Administration', 'manage_options', 'faf_admin_page_slug', 'faf_admin_page');
+    add_submenu_page('faf_admin_page_slug', 'leagues', 'Manage leagues', 0, 'faf_admin_leagues_page_slug', 'faf_admin_page_leagues');
     add_submenu_page('faf_admin_page_slug', 'players', 'Manage players', 0, 'faf_admin_players_page_slug', 'faf_admin_page_players');
 }
 
 function faf_admin_page() {
     echo "<h1>faf Administration Panel</h1>";
+}
+
+function faf_admin_page_leagues() {
+
+    // Declare global usages
+    global $wpdb;
+
+    // Declare vars
+    $pagename = 'faf_admin_leagues_page_slug';
+
+    // Process league removal
+    $removeId = $_GET['remove'];
+    if(is_numeric($removeId))
+    {
+        if($wpdb->delete($wpdb->prefix . 'faf_leagues', array('id' => $removeId)))
+            echo 'League ID ' . $removeId . ' was removed successfully';
+        else
+            echo 'An error occurred while removing league ID ' - $removeId;
+    }
+
+    // Process league insertion
+    if(isset($_POST['submit']))
+    {
+        if($wpdb->insert($wpdb->prefix . 'faf_leagues', array('name' => $_POST['league_name'], 'description' => $_POST['league_description'])))
+            echo 'League created successfully';
+        else
+            echo 'An error occurred while creating the new league';
+    }
+
+    // Prepare query for selecting leagues
+    $query = 'SELECT id, name, description FROM ' . $wpdb->prefix . 'faf_leagues';
+    $res = $wpdb->get_results($query);
+
+    // Prepare table header
+    echo '<table class="tablebox">';
+    echo '<tr>';
+        echo '<td>ID</td>';
+        echo '<td>League name</td>';
+        echo '<td>League description</td>';
+        echo '<td>Operations</td>';
+    echo '</tr>';
+
+    // Prepare table body
+    foreach($res as $league)
+    {
+        echo '<tr>';
+            echo '<td>' . $league->id . '</td>';
+            echo '<td>' . $league->name . '</td>';
+            echo '<td>' . $league->description . '</td>';
+            echo '<td>' . '<a href="?page=' . $pagename . '&remove=' . $league->id . '">Remove</a></td>';
+        echo '</tr>';
+    } 
+
+    // Prepare table footer
+    echo '</table>';
+
+    // Prepare new league form
+    echo '<form action="?page=' . $pagename . '" method="post" enctype="multipart/form-data">';
+        echo 'League name';
+        echo '<input type="text" name="league_name" id="league_name">';
+        echo 'League description';
+        echo '<input type="text" name="league_description" id="league_description">';
+        echo '<input type="hidden" name="submit">';
+        submit_button('Create league');
+    echo ' </form>';
 }
 
 function faf_admin_page_players() {
